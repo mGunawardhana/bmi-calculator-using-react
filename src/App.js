c7220b0201373a4e1acc2f0ef8048ca0447e9173
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-03-22 13:58:25 +0530

    content update

--- src/App.js
+++ src/App.js
@@ -22,14 +22,8 @@ function App() {
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
         Sign In
       </button>
-      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        Forgot Password?
-      </a>
     </div>
   </form>
-  <p class="text-center text-gray-500 text-xs">
-    &copy;2020 Acme Corp. All rights reserved.
-  </p>
 </div>
   );
 }
